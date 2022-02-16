6dd0d96ba1a43abae1b3b63b9395f9aa018aae72
FrancescoPiazzi <francescopiazzi25@gmail.com>
2022-02-16 11:39:03 +0100

    Rimosso makefile e dipendenze

--- 46_InvertiFile/InvertiFile.cpp
+++ 46_InvertiFile/InvertiFile.cpp
@@ -2,32 +2,80 @@ using namespace std;
 
 #include "iostream"
 #include "fstream"
-#include "pila.hpp"
+
+struct node{
+    node *next;
+    int value;
+};
+
+void put(node*&, int);
+node* initPila(int);
+void print(node*);
+node* get(node*&);
+void dealloc(node*&);
 
 int main(){
     fstream input, output;
+    node* pila;
     char buffer[256];
     int numero;
 
     input.open("input.txt", ios::in);
     output.open("output.txt", ios::out);
 
-    init();
+    if(!input.eof()){
+        input >> buffer;
+        pila = initPila(atoi(buffer));
+    }
 
     while(!input.eof()){
         input >> buffer;
-        push(atoi(buffer));
+        put(pila, atoi(buffer));
     }
 
-    while(top(numero)){
+    while(pila != NULL){
+        numero = get(pila)->value;
         output << numero << endl;
-        pop();
     }
 
-    deinit();
+    dealloc(pila);
 
     input.close();
     output.close();
 
 	return 0;
+}
+
+void put(node*& pila, int value){
+    node* newPila = new node{NULL, value};
+    newPila->next = pila;
+    pila = newPila;
+}
+
+node* initPila(int value){
+    node* pila = new node{NULL, value};
+    return pila;
+}
+
+void print(node* pila){
+    while(pila != NULL){
+        cout << pila->value << " ";
+        pila = pila->next;
+    }
+    cout << endl;
+}
+
+node* get(node*& pila){
+    node* nodeToReturn = pila;
+    pila = pila->next;
+    return nodeToReturn;
+}
+
+void dealloc(node*& pila){
+    node* tmp;
+    while(pila != NULL){
+        tmp = pila;
+        pila = pila->next;
+        delete tmp;
+    }
 }
